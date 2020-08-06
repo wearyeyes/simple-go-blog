4547d115d3a60885e0d852d2b153da3641f0142d
yuri laptev <61635175+yuriouch@users.noreply.github.com>
2020-08-07 00:34:51 +0300

    Add files via upload
    Add MongoDB storage

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module simple-go-blog
 
 go 1.14
 
-require github.com/gorilla/mux v1.7.4
+require (
+	github.com/gorilla/mux v1.7.4
+	go.mongodb.org/mongo-driver v1.4.0
+)
--- main.go
+++ main.go
@@ -2,20 +2,21 @@ package main
 
 import (
 	"html/template"
+	"log"
 	"net/http"
-	"time"
 
 	"github.com/gorilla/mux"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 // It's saves the template of our site in the cache to improve
-// performance and save memory. Now we don't need to do this every
-// time the function 'getPage' is called.
+// performance and save memory.
 var tmpl = template.Must(template.ParseFiles("assets/index.html"))
 
 type Post struct {
-	Text string
-	Time string
+	ID   primitive.ObjectID `json:"_id,omitempty" bson:"_id,omitempty"`
+	Text string             `json:"text" bson:"text"`
+	Time string             `json:"time" bson:"time"`
 }
 
 // Main struct.
@@ -23,43 +24,19 @@ type Page struct {
 	Posts []Post
 }
 
-// All our posts saves in this slice.
-var allPosts []Post
-
-func getPage(w http.ResponseWriter, req *http.Request) {
-	tmpl.Execute(w, Page{
-		Posts: allPosts,
-	})
-}
-
-func createPost(w http.ResponseWriter, req *http.Request) {
-	// Read text from textarea in html template.
-	text := req.FormValue("newpost")
-
-	if text != "" {
-		newPost := Post{
-			Text: text,
-			Time: time.Now().Format("2 Jan 15:04"),
-		}
-
-		// Insert a new post in start of our slice.
-		var p []Post
-		p = append(p, newPost)
-		allPosts = append(allPosts[:0], append(p, allPosts[0:]...)...)
-	}
-
-	http.Redirect(w, req, "/", http.StatusFound)
-}
-
 func main() {
+	// Create router...
 	r := mux.NewRouter()
 
 	// Connect css-file in directory assets/style.
 	fs := http.FileServer(http.Dir("assets"))
 	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets/", fs))
 
-	r.HandleFunc("/", getPage).Methods("GET")
-	r.HandleFunc("/", createPost).Methods("POST")
+	// Handlers.
+	r.HandleFunc("/", getAllPosts).Methods("GET")
+	r.HandleFunc("/", createNewPost).Methods("POST")
+
+	log.Println("Server start working...")
 
 	http.ListenAndServe(":8080", r)
 }
